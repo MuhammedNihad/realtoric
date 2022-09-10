e6bf5c5b9f1a9fe5bbb7da4cfa4da4be1b56512f
Muhammed Nihad <muhammednihad999@gmail.com>
2022-09-10 21:01:17 +0530

    Add: property model

--- realtoric/core/admin.py
+++ realtoric/core/admin.py
@@ -1,3 +1,17 @@
-# from django.contrib import admin
+from django.contrib import admin
 
-# Register your models here.
+from .models import Property
+
+
+@admin.register(Property)
+class PropertyAdmin(admin.ModelAdmin):
+    list_display = ("name", "type", "sale_status", "user", "created", "modified")
+    list_filter = ("type", "sale_status", "created", "modified")
+    search_fields = ("name", "type", "sale_status", "created", "modified")
+    date_hierarchy = "created"
+    ordering = ("-created",)
+
+    class Meta:
+        model = Property
+        verbose_name = "Property"
+        verbose_name_plural = "Properties"
--- realtoric/core/models.py
+++ realtoric/core/models.py
@@ -1,3 +1,96 @@
-# from django.db import models
+import itertools
 
-# Create your models here.
+from django.conf import settings
+from django.db import models
+from django.template.defaultfilters import slugify
+
+
+class TimeStampedModel(models.Model):
+    """
+    An abstract base class model that provides self-updating ``created`` and ``modified`` fields.
+    """
+
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+class Property(TimeStampedModel):
+    """
+    Inherited model for listing properties.
+    """
+
+    class PropertyType(models.TextChoices):
+        """
+        Property type choices.
+        """
+
+        HOUSE = "House"
+        APARTMENT = "Apartment"
+        VILLA = "Villa"
+        COMMERCIAL = "Commercial"
+        LAND = "Land"
+
+    class SaleStatus(models.TextChoices):
+        """
+        Sale status choices.
+        """
+
+        FOR_SALE = "For Sale"
+        SOLD = "Sold"
+        # FOR_RENT = 'For Rent'
+        # RENTED = 'Rented'
+
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="posted_by"
+    )
+    name = models.CharField(max_length=255)
+    description = models.TextField(max_length=512)
+    type = models.CharField(
+        max_length=50, choices=PropertyType.choices, default=PropertyType.HOUSE
+    )
+    address = models.CharField(max_length=255)
+    city = models.CharField(max_length=255)
+    google_maps = models.URLField(max_length=512)
+    image = models.ImageField(upload_to="properties", blank=True, null=True)
+    sale_status = models.CharField(
+        max_length=50, choices=SaleStatus.choices, default=SaleStatus.FOR_SALE
+    )
+    contact_info = models.CharField(max_length=255)
+    slug = models.SlugField(unique=True)
+
+    class Meta:
+        ordering = ["-created"]
+        verbose_name = "Property"
+        verbose_name_plural = "Properties"
+
+    def __str__(self):
+        return self.name
+
+    def _generate_slug(self):
+        "Generate a unique slug for each property."
+
+        # max_length = self._meta.get_field("slug").max_length
+        value = self.name
+        slug_candidate = slug_original = slugify(value)
+        for i in itertools.count(1):
+            if not Property.objects.filter(slug=slug_candidate).exists():
+                break
+            slug_candidate = f"{slug_original}-{i}"
+        self.slug = slug_candidate
+
+    def save(self, *args, **kwargs):
+        """
+        To avoid generating the slug each time you save an existing model,
+        detect if it is an update of the model on each call of model's save() method.
+        """
+        if not self.pk:
+            self._generate_slug()
+        super().save(*args, **kwargs)
+
+    def get_absolute_url(self):
+        "Return the absolute url of the property."
+        # return reverse('properties:detail', kwargs={'slug': self.slug})
+        return f"/property/{self.slug}/"
