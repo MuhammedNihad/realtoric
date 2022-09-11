39fa3daf6874c47222cfaf264cb74edc335f6f2a
Muhammed Nihad <muhammednihad999@gmail.com>
2022-09-11 18:06:35 +0530

    Add: name, address and phone to user signup form

--- realtoric/users/forms.py
+++ realtoric/users/forms.py
@@ -1,5 +1,6 @@
 from allauth.account.forms import SignupForm
 from allauth.socialaccount.forms import SignupForm as SocialSignupForm
+from django import forms
 from django.contrib.auth import forms as admin_forms
 from django.contrib.auth import get_user_model
 from django.utils.translation import gettext_lazy as _
@@ -32,6 +33,33 @@ class UserSignupForm(SignupForm):
     Default fields will be added automatically.
     Check UserSocialSignupForm for accounts created from social.
     """
+
+    name = forms.CharField(
+        max_length=255,
+        label=_("Name"),
+        widget=forms.TextInput(attrs={"placeholder": _("Name")}),
+    )
+    address = forms.CharField(
+        max_length=255,
+        label=_("Address"),
+        required=False,
+        widget=forms.Textarea(attrs={"placeholder": _("Enter your address here...")}),
+    )
+    phone = forms.CharField(
+        max_length=13,
+        label=_("Phone Number"),
+        widget=forms.TextInput(
+            attrs={"placeholder": _("Phone Number"), "type": "number"}
+        ),
+    )
+
+    def save(self, request):
+        user = super().save(request)
+        user.name = self.cleaned_data["name"]
+        user.address = self.cleaned_data["address"]
+        user.phone = self.cleaned_data["phone"]
+        user.save()
+        return user
 
 
 class UserSocialSignupForm(SocialSignupForm):
