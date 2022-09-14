0570e0efd7dbbd72fd06623deaa4155004e55b79
Muhammed Nihad <muhammednihad999@gmail.com>
2022-09-14 17:32:13 +0530

    Feat: show object id of users and property in admin panel

--- realtoric/core/admin.py
+++ realtoric/core/admin.py
@@ -5,7 +5,7 @@ from .models import Property
 
 @admin.register(Property)
 class PropertyAdmin(admin.ModelAdmin):
-    list_display = ("name", "type", "sale_status", "user", "created", "modified")
+    list_display = ("id", "name", "type", "sale_status", "user", "created", "modified")
     list_filter = ("type", "sale_status", "created", "modified")
     search_fields = ("name", "type", "sale_status", "created", "modified")
     date_hierarchy = "created"
--- realtoric/users/admin.py
+++ realtoric/users/admin.py
@@ -30,5 +30,5 @@ class UserAdmin(auth_admin.UserAdmin):
         ),
         (_("Important dates"), {"fields": ("last_login", "date_joined")}),
     )
-    list_display = ["username", "name", "is_superuser"]
+    list_display = ["id", "username", "name", "is_superuser"]
     search_fields = ["name"]
