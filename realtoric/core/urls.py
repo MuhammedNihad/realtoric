ff2bcf63685f213a91c62425edb71811244cf876
Muhammed Nihad <muhammednihad999@gmail.com>
2022-09-18 16:52:33 +0530

    Feat: render properties for sale in home page + implement fontawesome icons

--- realtoric/core/urls.py
+++ realtoric/core/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import post_property_view
+from .views import post_property_view, property_list_view
 
 app_name = "core"
 urlpatterns = [
+    path("", view=property_list_view, name="home"),
     path("post-property/", view=post_property_view, name="post_property"),
 ]
--- realtoric/core/views.py
+++ realtoric/core/views.py
@@ -2,9 +2,26 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.messages.views import SuccessMessageMixin
 from django.urls import reverse_lazy
 from django.utils.translation import gettext_lazy as _
-from django.views.generic.edit import CreateView
+from django.views.generic import CreateView, ListView
 
 from .forms import PostPropertyForm
+from .models import Property
+
+
+class PropertyListView(ListView):
+    """
+    Render properties for sale in home page.
+    """
+
+    model = Property
+    context_object_name = "properties"
+    queryset = Property.objects.filter(
+        sale_status=Property.SaleStatus.FOR_SALE
+    ).order_by("-created")[0:30]
+    template_name = "pages/home.html"
+
+
+property_list_view = PropertyListView.as_view()
 
 
 class PostPropertyView(LoginRequiredMixin, SuccessMessageMixin, CreateView):
