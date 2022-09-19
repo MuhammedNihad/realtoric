a79ab6324b89c68140600db126d209be2d9cf18d
Muhammed Nihad <muhammednihad999@gmail.com>
2022-09-19 23:31:22 +0530

    Feat: add property search functionality

--- realtoric/core/urls.py
+++ realtoric/core/urls.py
@@ -1,10 +1,16 @@
 from django.urls import path
 
-from .views import post_property_view, property_detail_view, property_list_view
+from .views import (
+    post_property_view,
+    property_detail_view,
+    property_list_view,
+    property_search_results_view,
+)
 
 app_name = "core"
 urlpatterns = [
     path("", view=property_list_view, name="home"),
+    path("search/", view=property_search_results_view, name="search_results"),
     path("property/<slug:slug>/", view=property_detail_view, name="property_detail"),
     path("post-property/", view=post_property_view, name="post_property"),
 ]
--- realtoric/core/views.py
+++ realtoric/core/views.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.messages.views import SuccessMessageMixin
+from django.db.models import Q
 from django.urls import reverse_lazy
 from django.utils.translation import gettext_lazy as _
 from django.views.generic import CreateView, DetailView, ListView
@@ -22,6 +23,32 @@ class PropertyListView(ListView):
 
 
 property_list_view = PropertyListView.as_view()
+
+
+class PropertySearchResultsView(ListView):
+    """
+    Render search results.
+    """
+
+    model = Property
+    context_object_name = "properties"
+    template_name = "pages/search_results.html"
+
+    def get_queryset(self):
+        """
+        Return search results.
+        """
+
+        query = self.request.GET.get("search")
+        return Property.objects.filter(
+            Q(name__icontains=query)
+            | Q(description__icontains=query)
+            | Q(address__icontains=query)
+            | Q(city__icontains=query)
+        ).order_by("-created")
+
+
+property_search_results_view = PropertySearchResultsView.as_view()
 
 
 class PropertyDetailView(DetailView):
