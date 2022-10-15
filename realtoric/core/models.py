f71e8c6667c66a9b009f8b5f5b10b59600781583
Muhammed Nihad <muhammednihad@pm.me>
2022-10-15 17:33:38 +0530

    Refactor: remove few model fields as temporary

--- realtoric/core/models.py
+++ realtoric/core/models.py
@@ -7,27 +7,6 @@ from django.template.defaultfilters import slugify
 User = get_user_model()
 
 
-class CityChoice(models.TextChoices):
-    """
-    Choices for the city field.
-    """
-
-    ERNAKULAM = "Ernakulam"
-    KANNUR = "Kannur"
-    KOCHI = "Kochi"
-    KOZHIKODE = "Kozhikode"
-    THIRUVANANTHAPURAM = "Thiruvananthapuram"
-
-
-class SellOrRent(models.TextChoices):
-    """
-    Choices for the sell or rent field.
-    """
-
-    SELL = "Sell"
-    RENT = "Rent"
-
-
 class SaleStatus(models.TextChoices):
     """
     Sale status of the property.
@@ -35,15 +14,6 @@ class SaleStatus(models.TextChoices):
 
     FOR_SALE = "For Sale"
     SOLD = "Sold"
-
-
-class RentStatus(models.TextChoices):
-    """
-    Rent status of the property.
-    """
-
-    FOR_RENT = "For Rent"
-    RENTED = "Rented"
 
 
 class CountChoice(models.TextChoices):
@@ -70,65 +40,6 @@ class CountWithZeroChoice(models.TextChoices):
     THREE_PLUS = "3+"
 
 
-class FurnishingStatus(models.TextChoices):
-    """
-    Furnishing status of the property.
-    """
-
-    FURNISHED = "Furnished"
-    SEMI_FURNISHED = "Semi-Furnished"
-    UNFURNISHED = "Unfurnished"
-
-
-class ConstructionStatus(models.TextChoices):
-    """
-    Construction status of the property.
-    """
-
-    NEW_LAUNCH = "New Launch"
-    READY_TO_MOVE = "Ready to Move"
-    UNDER_CONSTRUNCTION = "Under Construction"
-
-
-class ListedBy(models.TextChoices):
-    """
-    Choices for who listed the property.
-    """
-
-    OWNER = "Owner"
-    BUILDER = "Builder"
-    DEALER = "Dealer"
-
-
-class Facing(models.TextChoices):
-    """
-    Choices for the facing of the property.
-    """
-
-    EAST = "East"
-    NORTH = "North"
-    NORTH_EAST = "North East"
-    NORTH_WEST = "North West"
-    SOUTH = "South"
-    SOUTH_EAST = "South East"
-    SOUTH_WEST = "South West"
-    WEST = "West"
-
-
-class SocialAmenity(models.TextChoices):
-    """
-    Choices for social amenities.
-    """
-
-    GYM = "Gym"
-    PARK = "Park"
-    GARDEN = "Garden"
-    SECURITY = "Security"
-    PLAY_AREA = "Play Area"
-    POWER_BACKUP = "Power Backup"
-    SWIMMING_POOL = "Swimming Pool"
-
-
 class TimeStampMixin(models.Model):
     """
     Abstract model class for time stamping.
@@ -152,11 +63,8 @@ class PropertyBasicDetailMixin(models.Model):
     description = models.CharField(
         max_length=128, help_text="Include condition, reason for selling, etc."
     )
-    city = models.CharField(max_length=50, choices=CityChoice.choices)
+    city = models.CharField(max_length=50)
     address = models.CharField(max_length=128)
-    landmark = models.CharField(
-        max_length=128, blank=True, verbose_name="Landmark (Optional)"
-    )
     google_map = models.URLField(blank=True, verbose_name="Google Maps Link (Optional)")
     contact_info = models.CharField(
         max_length=128,
@@ -164,23 +72,11 @@ class PropertyBasicDetailMixin(models.Model):
         verbose_name="Contact number or email (Optional)",
         help_text="Your default phone number or email will be used if this field is not filled.",
     )
-    sell_or_rent = models.CharField(
-        max_length=20,
-        choices=SellOrRent.choices,
-        default=SellOrRent.SELL,
-        verbose_name="List Property For",
-    )
     sale_status = models.CharField(
         max_length=20,
         choices=SaleStatus.choices,
         default=SaleStatus.FOR_SALE,
         verbose_name="Sale Status",
-    )
-    rent_status = models.CharField(
-        max_length=20,
-        choices=RentStatus.choices,
-        default=RentStatus.FOR_RENT,
-        verbose_name="Rent Status",
     )
     slug = models.SlugField(unique=True)
 
@@ -209,53 +105,8 @@ class Apartment(PropertyBasicDetailMixin, TimeStampMixin):
         default=CountChoice.ONE,
         verbose_name="Bathrooms",
     )
-    furnishing = models.CharField(
-        max_length=20,
-        choices=FurnishingStatus.choices,
-        default=FurnishingStatus.FURNISHED,
-        verbose_name="Furnishing",
-    )
-    construction_status = models.CharField(
-        max_length=20,
-        choices=ConstructionStatus.choices,
-        default=ConstructionStatus.NEW_LAUNCH,
-        verbose_name="Construction Status",
-    )
-    listed_by = models.CharField(
-        max_length=20,
-        choices=ListedBy.choices,
-        default=ListedBy.OWNER,
-        verbose_name="Listed by",
-    )
-    car_parking = models.CharField(
-        max_length=20,
-        choices=CountWithZeroChoice.choices,
-        default=CountWithZeroChoice.ZERO,
-        verbose_name="Car Parking",
-    )
-    facing = models.CharField(
-        max_length=20,
-        choices=Facing.choices,
-        default=Facing.EAST,
-        verbose_name="Facing",
-    )
-    no_of_balcony = models.CharField(
-        max_length=20,
-        choices=CountWithZeroChoice.choices,
-        default=CountWithZeroChoice.ZERO,
-        verbose_name="No. of Balconies",
-    )
-    society_amenity = models.CharField(
-        max_length=20,
-        choices=SocialAmenity.choices,
-        default=SocialAmenity.POWER_BACKUP,
-        verbose_name="Society Amenities",
-    )
-    super_built_up_area = models.PositiveIntegerField(
-        default=0, verbose_name="Super Built-up Area (sqft)"
-    )
-    carpet_area = models.PositiveIntegerField(
-        default=0, verbose_name="Carpet Area (sqft)"
+    area = models.PositiveIntegerField(
+        default=0, verbose_name="Area (sqft)"
     )
     floor = models.PositiveSmallIntegerField(default=0, verbose_name="Total Floors")
 
@@ -351,37 +202,10 @@ class Commercial(PropertyBasicDetailMixin, TimeStampMixin):
         default=CommercialType.SHOP,
         verbose_name="Commercial Type",
     )
-    furnishing = models.CharField(
-        max_length=20,
-        choices=FurnishingStatus.choices,
-        default=FurnishingStatus.FURNISHED,
-        verbose_name="Furnishing",
-    )
-    construction_status = models.CharField(
-        max_length=20,
-        choices=ConstructionStatus.choices,
-        default=ConstructionStatus.NEW_LAUNCH,
-        verbose_name="Construction Status",
-    )
-    listed_by = models.CharField(
-        max_length=20,
-        choices=ListedBy.choices,
-        default=ListedBy.OWNER,
-        verbose_name="Listed by",
-    )
-    super_built_up_area = models.PositiveIntegerField(
-        default=0, verbose_name="Super Built-up Area (sqft)"
-    )
-    carpet_area = models.PositiveIntegerField(
-        default=0, verbose_name="Carpet Area (sqft)"
+    area = models.PositiveIntegerField(
+        default=0, verbose_name="Area (sqft)"
     )
     floor = models.PositiveSmallIntegerField(default=0, verbose_name="Total Floors")
-    washrooms = models.CharField(
-        max_length=20,
-        choices=CountWithZeroChoice.choices,
-        default=CountWithZeroChoice.ZERO,
-        verbose_name="Washrooms",
-    )
 
     class Meta:
         ordering = ["-listed_on"]
@@ -477,41 +301,8 @@ class House(PropertyBasicDetailMixin, TimeStampMixin):
         default=CountChoice.ONE,
         verbose_name="Bathrooms",
     )
-    furnishing = models.CharField(
-        max_length=20,
-        choices=FurnishingStatus.choices,
-        default=FurnishingStatus.FURNISHED,
-        verbose_name="Furnishing",
-    )
-    construction_status = models.CharField(
-        max_length=20,
-        choices=ConstructionStatus.choices,
-        default=ConstructionStatus.NEW_LAUNCH,
-        verbose_name="Construction Status",
-    )
-    listed_by = models.CharField(
-        max_length=20,
-        choices=ListedBy.choices,
-        default=ListedBy.OWNER,
-        verbose_name="Listed by",
-    )
-    car_parking = models.CharField(
-        max_length=20,
-        choices=CountWithZeroChoice.choices,
-        default=CountWithZeroChoice.ZERO,
-        verbose_name="Car Parking",
-    )
-    facing = models.CharField(
-        max_length=20,
-        choices=Facing.choices,
-        default=Facing.NORTH,
-        verbose_name="Facing",
-    )
-    super_built_up_area = models.PositiveIntegerField(
-        default=0, verbose_name="Super Built-up Area (sqft)"
-    )
-    carpet_area = models.PositiveIntegerField(
-        default=0, verbose_name="Carpet Area (sqft)"
+    area = models.PositiveIntegerField(
+        default=0, verbose_name="Area (sqft)"
     )
     floor = models.PositiveSmallIntegerField(default=0, verbose_name="Total Floors")
 
@@ -708,41 +499,8 @@ class Villa(PropertyBasicDetailMixin, TimeStampMixin):
         default=CountChoice.ONE,
         verbose_name="Bathrooms",
     )
-    furnishing = models.CharField(
-        max_length=20,
-        choices=FurnishingStatus.choices,
-        default=FurnishingStatus.FURNISHED,
-        verbose_name="Furnishing",
-    )
-    construction_status = models.CharField(
-        max_length=20,
-        choices=ConstructionStatus.choices,
-        default=ConstructionStatus.NEW_LAUNCH,
-        verbose_name="Construction Status",
-    )
-    listed_by = models.CharField(
-        max_length=20,
-        choices=ListedBy.choices,
-        default=ListedBy.OWNER,
-        verbose_name="Listed by",
-    )
-    car_parking = models.CharField(
-        max_length=20,
-        choices=CountWithZeroChoice.choices,
-        default=CountWithZeroChoice.ZERO,
-        verbose_name="Car Parking",
-    )
-    facing = models.CharField(
-        max_length=20,
-        choices=Facing.choices,
-        default=Facing.NORTH,
-        verbose_name="Facing",
-    )
-    super_built_up_area = models.PositiveIntegerField(
-        default=0, verbose_name="Super Built-up Area (sqft)"
-    )
-    carpet_area = models.PositiveIntegerField(
-        default=0, verbose_name="Carpet Area (sqft)"
+    area = models.PositiveIntegerField(
+        default=0, verbose_name="Area (sqft)"
     )
     floor = models.PositiveSmallIntegerField(default=0, verbose_name="Total Floors")
 
